46701f2988271745c6ecf26d4e3b4e2e5679ae11
Akash <akash.grep@gmail.com>
2020-09-14 16:22:20 +0530

    added cors , bodyaparser mounted the existing apiRouter at /api added PORT to listen on the PORT

--- server.js
+++ server.js
@@ -9,17 +9,21 @@ module.exports = app;
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
-
+var cors = require('cors');
+app.use(cors);
 
 // Add middware for parsing request bodies here:
-
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
-
+app.use('/api', apiRouter);
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
-
+app.listen(PORT,()=>{
+console.log(`Server listen on ${PORT}`);
+});
 }
